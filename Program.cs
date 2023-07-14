e69bc409034c44ae4016658bcee07c5b36b2c9cc
dan3988 <daniel.pickett@hotmail.co.uk>
2023-07-14 11:58:08 +0100

    Added event viewer logging

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 using DnsUpdater;
 
+using Microsoft.Extensions.Logging.EventLog;
+
 IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureLogging((ctx, logging) =>
+	{
+		if (OperatingSystem.IsWindows())
+		{
+			var options = ctx.Configuration.GetSection("Logging:EventLog").Get<EventLogSettings>();
+			logging.AddEventLog(options ?? new());
+		}
+	})
 	.ConfigureServices(services =>
 	{
 		services.AddHttpClient();
